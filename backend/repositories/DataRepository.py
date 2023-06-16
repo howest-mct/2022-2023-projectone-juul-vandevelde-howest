c9c648b3d4cc420bef7e78411670801ebe9b16a0
Juul Van de Velde <juul.van.de.velde@student.howest.be>
2023-06-16 11:48:23 +0200

    added base code for timeline on dashboard

--- backend/repositories/DataRepository.py
+++ backend/repositories/DataRepository.py
@@ -16,22 +16,23 @@ class DataRepository:
         return Database.get_rows(sql)
 
     @staticmethod
-    def read_devices():
-        sql = "SELECT * FROM device"
-        return Database.get_rows(sql)
+    def get_user_id(rfid_id):
+        sql = "SELECT user_id FROM user WHERE rfid_id = %s"
+        params = [rfid_id]
+        return Database.get_one_row(sql, params)
 
     @staticmethod
     def read_device_history(device_id):
         sql = "SELECT * FROM history WHERE device_id = %s ORDER BY history_id DESC LIMIT 15"
         params = [device_id]
         return Database.get_rows(sql, params)
-    
+
     @staticmethod
     def read_device_history_today(device_id):
         sql = "SELECT * FROM history WHERE device_id = %s AND DATE(datetime) = CURDATE() ORDER BY history_id DESC"
         params = [device_id]
         return Database.get_rows(sql, params)
-    
+
     @staticmethod
     def read_unlocks_per_user():
         sql = "SELECT u.first_name, COUNT(history.history_id) AS counter FROM user u LEFT JOIN history ON history.value = u.rfid_id AND history.device_id = 9 GROUP BY u.first_name ORDER BY history.history_id DESC"
@@ -57,9 +58,19 @@ class DataRepository:
 
     @staticmethod
     def check_login(username, password):
-        sql = "SELECT COUNT(*) AS 'login_status' FROM user WHERE username = %s AND BINARY password = %s"
+        sql = "SELECT COUNT(*) AS 'login_status', user_id  FROM user WHERE username = %s AND BINARY password = %s"
         params = [username, password]
         return Database.get_one_row(sql, params)
+
+    @staticmethod
+    def check_mailbox_contents():
+        sql = "SELECT CASE WHEN device_id IN (4, 5) THEN 1 WHEN device_id = 9 THEN 0 ELSE NULL END AS mail_status FROM history WHERE device_id IN (4, 5, 9) ORDER BY history_id DESC LIMIT 1"
+        return Database.get_one_row(sql)
+
+    @staticmethod
+    def get_timeline():
+        sql = "SELECT h.device_id, h.datetime, CASE WHEN h.device_id = 9 THEN u.username ELSE h.value END AS value FROM history h LEFT JOIN user u ON h.device_id = 9 AND h.value = u.user_id WHERE h.device_id IN (3, 4, 5, 9) AND (h.device_id <> 3 OR (h.device_id = 3 AND h.value > 30 AND h.history_id = (SELECT MAX(history_id) FROM history WHERE device_id = 3 LIMIT 1))) ORDER BY h.history_id DESC LIMIT 5;"
+        return Database.get_rows(sql)
 
     @staticmethod
     def read_current_color():
